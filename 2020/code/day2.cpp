18d0687417b4db3ebd56e5d77dc4dc218f2d37d1
IceDops <johannesnydahl1@gmail.com>
2020-12-02 14:00:26 +0100

    Removed some unnecessary logging

--- 2020/code/day2.cpp
+++ 2020/code/day2.cpp
@@ -12,10 +12,8 @@ int main() {
 
     std::ifstream infile("../input/day2.txt");
     while (infile >> f >> k >> s) {
-        std::cout << f << k << s << std::endl;
         std::string pref = f.substr(0, f.find("-"));
         f.erase(0, f.find("-") + 1);
-        std::cout << pref << f << std::endl;
 
         std::stringstream toint(pref);
         std::stringstream toint2(f);
@@ -24,7 +22,7 @@ int main() {
 
 
         a = std::count(s.begin(), s.end(), k[0]);
-        std::cout << a << std::endl;
+
         if ((a >= range) && (a <= range2)) b++;
         if (s[range - 1] == k[0] ^ s[range2 - 1] == k[0]) x++;
 
