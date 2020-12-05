aed98fd248b8c17ac8fb50b18dc34c7b5762ea4c
Johannes <johannesnydahl1@gmail.com>
2020-12-05 02:42:34 +0100

    Day 4 part 1

--- 2020/code/day4.cpp
+++ 2020/code/day4.cpp
@@ -6,30 +6,45 @@
 std::string input;
 std::string thing;
 int i = 0;
-int liness = 0;
+int lines = 0;
+int a = 0;
+int f = 0;
 int main() {
+	std::cout << "kek" << std::endl;
 	std::ifstream file("../input/day4.txt");
-	//count lines and create array
+	//count passports
 	while (getline(file, thing)) {
-		liness++;
-	
+		if (thing.size() == 0) {
+			f++;
+		}
 	}
-	const int lines = liness;
-	
-	std::string fullinput[lines] = { };
+	std::cout << f << std::endl;
+	std::ifstream filee("../input/day4.txt");
+	std::cout << lines << std::endl;
 
-	while (getline(file, input)) {
+
+	std::string* fullinput = new std::string[f + 1]{ };
+	while (getline(filee, input)) {
 		if (input.size() > 0) {
-			fullinput[i] = input;
-			std::cout << fullinput[i] << std::endl;
+			fullinput[i] += " " + input;
 		}
 		else {
+			std::cout << fullinput[i] << i << "feg" << std::endl;
 			i++;
-			std::cout << "Yes!" << std::endl;
 		}
+	}
+	std::cout << fullinput[i] << i << "mög" << std::endl;
+	std::cout << fullinput[256] << i << "cringe" <<  std::endl;
+		for (int k = 0; k < f; k++) {
+			if (fullinput[k].find("byr") != std::string::npos && fullinput[k].find("iyr") != std::string::npos && fullinput[k].find("eyr") != std::string::npos && fullinput[k].find("hgt") != std::string::npos && fullinput[k].find("hcl") != std::string::npos && fullinput[k].find("ecl") != std::string::npos && fullinput[k].find("pid") != std::string::npos) {
+				a++;
+
+			}
+
+		}
+		std::cout << "Correct passports: " << a << std::endl;
+
 
 	}
-	for (int k = 0; k > fullinput.size(); k++) {
-		std::cout << fullinput[k];
-	}
-}
+
+//
