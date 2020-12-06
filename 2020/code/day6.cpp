171844eeff1e74c51cfd55c8bae51ae77effff3d
Johannes <johannesnydahl1@gmail.com>
2020-12-06 14:17:24 +0100

    Day 6

---
+++ 2020/code/day6.cpp
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include<stdio.h>
+
+std::string input;
+int e[26] = { 0 };
+int sum = 0;
+int members = 0;
+
+int main() {
+	std::ifstream file("../input/day6.txt");
+	while (getline(file, input)) {
+		std::cout << input << std::endl;
+		std::cout << input.size() << std::endl;
+		if (input.size() != 0) {
+			for (int i = 97; i < 123; i++) {
+				if (input.find(char(i)) != std::string::npos) {
+					e[i - 97]++;
+
+				}
+
+
+			}
+			members++;
+		}
+		else if (input.size() == 0) {
+			std::cout << "#" << std::endl;
+			for (int f = 0; f < sizeof(e) / sizeof(*e); f++) {
+				if (e[f] == members) {
+					sum++;
+				}
+
+			}
+
+
+			std::cout << "Sum : " << sum << std::endl;
+			members = 0;
+			for (int y = 0; y < 26; y++) {
+				e[y] = 0;
+			}
+
+
+		}
+
+	}
+	std::cout << sum << std::endl;
+}
